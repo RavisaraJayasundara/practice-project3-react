02797e5ab51b9c8f2b843fb75f37b3914646c927
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-29 16:51:48 +0530

    update

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,26 +1,25 @@
+import './assets/css/compo.css';
 import {useState} from 'react';
-
 function App() {
-   const[fontSize,setFontSize]=useState(16);
-
-  const small=()=>{
-    setFontSize("16px")
-  }
-  const medium=()=>{
-    setFontSize("20px")
-  }
-  const large=()=>{
-    setFontSize("24px")
-  }
+  const[fontSize,setfontSize]=useState();
+   const small=()=>{
+    setfontSize("16px");
+   }
+   const medium=()=>{
+    setfontSize("20px")
+   }
+   const large=()=>{
+    setfontSize("24px")
+   }
   return (
     <div style={{fontSize}}>
-      <h1 style={{textAlign:'center'}}>Student Information Portal</h1>
+      <h1>Student Information Portal</h1>
       <hr/>
-      Font size:
+      Font Size:
       <button onClick={small}>S</button>
       <button onClick={medium}>M</button>
       <button onClick={large}>L</button>
-       <hr/>
+      <hr/>
     </div>
   );
 }
