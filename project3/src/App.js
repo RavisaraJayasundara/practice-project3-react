e5d8b6786631c04d67b8da40fc65d8faaab0c710
Ravisara Jayasundara <139541406+RavisaraJayasundara@users.noreply.github.com>
2024-11-29 16:34:53 +0530

    font size aranging

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from 'react';
 
 function App() {
+   const[fontSize,setFontSize]=useState(16);
+
+  const small=()=>{
+    setFontSize("16px")
+  }
+  const medium=()=>{
+    setFontSize("20px")
+  }
+  const large=()=>{
+    setFontSize("24px")
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{fontSize}}>
+      <h1 style={{textAlign:'center'}}>Student Information Portal</h1>
+      <hr/>
+      Font size:
+      <button onClick={small}>S</button>
+      <button onClick={medium}>M</button>
+      <button onClick={large}>L</button>
+       <hr/>
     </div>
   );
 }
